6083d723fc00d25c7704bfebfa5779d89b86fd48
Nick <Nick@Morty.local>
2020-05-19 12:58:23 +1000

    lol comments

---
+++ synchronise-folder-with-s3.js
@@ -0,0 +1,36 @@
+var AWS = require('aws-sdk');
+var path = require("path");
+var fs = require('fs');
+
+const uploadDir = function(s3Path, bucketName) {
+
+    let s3 = new AWS.S3();
+
+    function walkSync(currentDirPath, callback) {
+        fs.readdirSync(currentDirPath).forEach(function (name) {
+            var filePath = path.join(currentDirPath, name);
+            var stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                callback(filePath, stat);
+            } else if (stat.isDirectory()) {
+                walkSync(filePath, callback);
+            }
+        });
+    }
+
+    walkSync(s3Path, function(filePath, stat) {
+        let bucketPath = filePath.substring(s3Path.length+1);
+        let params = {Bucket: bucketName, Key: bucketPath, Body: fs.readFileSync(filePath) };
+        s3.putObject(params, function(err, data) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Successfully uploaded '+ bucketPath +' to ' + bucketName);
+            }
+        });
+
+    });
+};
+
+uploadDir("node_modules", "bitwise-nick");
+
